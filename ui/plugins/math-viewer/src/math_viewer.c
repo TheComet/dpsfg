0434039c1e26f5ba1562a83f9c7f11d647891242
TheComet <alex.murray@gmx.ch>
2026-06-06 01:20:57 +0200

    Formatting

--- ui/plugins/math-viewer/src/math_viewer.c
+++ ui/plugins/math-viewer/src/math_viewer.c
@@ -5,12 +5,12 @@
 
 struct _MathViewer
 {
-    GtkBox     parent_instance;
+    GtkBox parent_instance;
     GtkWidget* drawing_area;
 
-    const struct csfg_tf_expr*   tf;
+    const struct csfg_tf_expr* tf;
     const struct csfg_expr_pool* pool;
-    int                          expr;
+    int expr;
 };
 
 G_DEFINE_DYNAMIC_TYPE(MathViewer, math_viewer, GTK_TYPE_BOX)
@@ -18,16 +18,16 @@ G_DEFINE_DYNAMIC_TYPE(MathViewer, math_viewer, GTK_TYPE_BOX)
 /* -------------------------------------------------------------------------- */
 static void draw_expr(cairo_t* cr, const struct csfg_expr_pool* pool, int expr)
 {
-    struct str*           str;
-    PangoLayout*          layout;
+    struct str* str;
+    PangoLayout* layout;
     PangoFontDescription* desc;
-    int                   tw1, th1;
+    int tw1, th1;
 
     str_init(&str);
     cairo_set_source_rgb(cr, 0, 0, 0);
 
     layout = pango_cairo_create_layout(cr);
-    desc = pango_font_description_from_string("Sans 16");
+    desc   = pango_font_description_from_string("Sans 16");
     pango_layout_set_font_description(layout, desc);
 
     csfg_expr_to_str(&str, pool, expr);
@@ -43,23 +43,23 @@ static void draw_expr(cairo_t* cr, const struct csfg_expr_pool* pool, int expr)
 
 /* -------------------------------------------------------------------------- */
 static void draw_tf(
-    cairo_t*                     cr,
+    cairo_t* cr,
     const struct csfg_expr_pool* pool,
-    const struct csfg_tf_expr*   tf)
+    const struct csfg_tf_expr* tf)
 {
-    struct str*           str;
-    PangoLayout*          layout;
+    struct str* str;
+    PangoLayout* layout;
     PangoFontDescription* desc;
-    int                   tw, th;
-    int                   widest;
-    double                tx = 0.0;
-    double                ty = 0.0;
+    int tw, th;
+    int widest;
+    double tx = 0.0;
+    double ty = 0.0;
 
     str_init(&str);
     cairo_set_source_rgb(cr, 0, 0, 0);
 
     layout = pango_cairo_create_layout(cr);
-    desc = pango_font_description_from_string("Sans 16");
+    desc   = pango_font_description_from_string("Sans 16");
     pango_layout_set_font_description(layout, desc);
 
     csfg_poly_expr_to_str(&str, pool, tf->num);
@@ -90,10 +90,10 @@ static void draw_tf(
 /* -------------------------------------------------------------------------- */
 static void draw_cb(
     GtkDrawingArea* area,
-    cairo_t*        cr,
-    int             width,
-    int             height,
-    gpointer        user_pointer)
+    cairo_t* cr,
+    int width,
+    int height,
+    gpointer user_pointer)
 {
     MathViewer* viewer = user_pointer;
     (void)area, (void)width, (void)height;
@@ -138,7 +138,7 @@ static void math_viewer_init(MathViewer* self)
 }
 
 /* -------------------------------------------------------------------------- */
-static void math_viewer_class_init(MathViewerClass* lass)
+static void math_viewer_class_init(MathViewerClass* class)
 {
 }
 
@@ -164,7 +164,7 @@ MathViewer* math_viewer_new(void)
 void math_viewer_set_expr(
     MathViewer* viewer, const struct csfg_expr_pool* pool, int expr)
 {
-    viewer->tf = NULL;
+    viewer->tf   = NULL;
     viewer->pool = pool;
     viewer->expr = expr;
     gtk_widget_queue_draw(viewer->drawing_area);
@@ -172,11 +172,11 @@ void math_viewer_set_expr(
 
 /* -------------------------------------------------------------------------- */
 void math_viewer_set_tf(
-    MathViewer*                  viewer,
+    MathViewer* viewer,
     const struct csfg_expr_pool* pool,
-    const struct csfg_tf_expr*   tf)
+    const struct csfg_tf_expr* tf)
 {
-    viewer->tf = tf;
+    viewer->tf   = tf;
     viewer->pool = pool;
     viewer->expr = -1;
     gtk_widget_queue_draw(viewer->drawing_area);
